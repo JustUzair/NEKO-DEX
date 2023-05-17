cd579e7757d348a1ba452cd9b05f4f9b678139e8
Uzair Saiyed <uzairhajra76330@gmail.com>
2023-05-17 22:46:08 +0530

    Added Login using RainbowKit Wallet Functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,67 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import Head from "next/head";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { NotificationProvider } from "web3uikit";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
+import { polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { MoralisProvider } from "react-moralis";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {}, []);
+  const router = useRouter();
+  const { chains, provider } = configureChains(
+    [polygonMumbai], // Testnet LINK  address : 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+    [publicProvider()]
+  );
+  const { connectors } = getDefaultWallets({
+    appName: "Neko-DEX",
+    chains,
+  });
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <MoralisProvider initializeOnMount={false}>
+          <NotificationProvider>
+            <AnimatePresence mode="wait" />
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  delay: 0.25,
+                  duration: 0.5,
+                },
+              }}
+              exit={{
+                opacity: 0,
+                backgroundColor: "transparent",
+
+                transition: {
+                  delay: 0.25,
+                  ease: "easeInOut",
+                },
+              }}
+              key={router.route}
+              className="content"
+            >
+              <Component {...pageProps} />
+            </motion.div>
+          </NotificationProvider>
+        </MoralisProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,83 @@
-import React from 'react'
-import { Game } from './game.js'
-import { useState } from 'react'
-
+import React, { useEffect } from "react";
+import { Game } from "./game.js";
+import { useState } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import { useNotification } from "web3uikit";
+import { ethers } from "ethers";
 export default function Home() {
-
-  const [login, setLogin] = useState(true);
-
+  const [login, setLogin] = useState(false);
+  const dispatch = useNotification();
+  const { runContractFunction } = useWeb3Contract();
+  const { enableWeb3, authenticate, account, isWeb3Enabled } = useMoralis();
+  const { chainId: chainIdHex } = useMoralis();
+  const chainId = parseInt(chainIdHex);
+  useEffect(() => {
+    enableWeb3();
+  }, []);
+  useEffect(() => {
+    authenticate();
+    console.log(`Account : ${account}`);
+    if (account != null) setLogin(true);
+    else setLogin(false);
+  }, [account]);
   const Login = () => {
     return (
-        <div className="container">
-          <div className="login">
-        <h1> NEKO DEX Cafe </h1>
-        Please connect your wallet to continue
-        <br/>
-        <button className="login-button" onClick={() => setLogin(false)}> Login </button>
-        </div>
-        </div>
-        
-    );
-    }
-  if(login) return (<Login/>
-  )
-  else
-  return (<Game/>)
+      <div className="container">
+        <div className="login">
+          <span
+            className="title-container"
+            style={{
+              marginTop: "25px",
+            }}
+          >
+            <h1 className="dapp-title"> NEKO DEX Cafe </h1>
+          </span>
+          <div className="connect-button--container">
+            <span
+              style={{
+                //   background: "rgba(232,63,121,1)",
+                background: "linear-gradient(to right, #E84C3D, #FF4E3F)",
+                padding: "0px 50px",
+                color: "#fff",
+                borderRadius: "15px",
+                width: "fit-content",
+                marginBottom: "25px",
+              }}
+            >
+              Please connect your wallet to continue
+            </span>
 
+            <div
+              style={{
+                width: "100%",
+              }}
+            >
+              <div
+                style={{
+                  margin: "0 auto",
+                  width: "145px",
+                }}
+              >
+                <ConnectButton
+                  accountStatus={{
+                    smallScreen: "avatar",
+                    largeScreen: "full",
+                  }}
+                  showBalance={{
+                    smallScreen: false,
+                    largeScreen: true,
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  //   if (account) return ;
+  //   else return ;
+  return <>{login ? <Game /> : <Login />}</>;
+  //   return <Login />;
 }
