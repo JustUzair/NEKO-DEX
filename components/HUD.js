fe9265fa9ffcbeb2344deccf22d30b9870d30a4b
Uzair Saiyed <uzairhajra76330@gmail.com>
2023-07-03 14:35:54 +0530

    Added fantom opera mainnet addresses

--- components/HUD.js
+++ components/HUD.js
@@ -9,78 +9,78 @@ export const HUD = ({
 }) => {
   return (
     <div className="HUD">
-      {chainId != null &&
-        (chainId == 80001 ||
-          chainId == 137 ||
-          chainId == 4002 ||
-          chainId == 250) && (
-          <div className="global-donations">
-            Global donations : $
-            {globalDonationsPolygon && globalDonationsPolygon[0]}
-          </div>
-        )}
-
-      {chainId != null && (chainId == 65 || chainId == 66) && (
-        <div className="global-donations okc-global--donation">
-          Global donations (Hover here)
-          <br />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-              flexDirection: "column",
-            }}
-          >
-            {globalDonationsOKX.length > 0 &&
-              globalDonationsOKX.map((item, index) => (
-                <span
-                  key={index}
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    padding: "0 20px",
-                  }}
-                >
-                  <span
-                    style={{
-                      width: "40%",
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      textAlign: "left",
-                    }}
-                  >
-                    {item?.donationTokenSymbol}{" "}
-                    <img
-                      src={item?.donationTokenImage}
-                      alt={item?.donationTokenSymbol}
-                      width={"20px"}
-                    />
-                  </span>{" "}
-                  <span
-                    style={{
-                      fontWeight: "500",
-                      textDecoration: "underline",
-                      cursor: "pointer",
-                      textAlign: "left",
-                    }}
-                    title={item?.donatedTokenAmount}
-                  >
-                    {" "}
-                    {item?.donatedTokenAmount.substr(0, 8) + "..."}
-                  </span>
-                </span>
-              ))}
-            <span
-              style={{
-                margin: "0 auto",
-              }}
-            >
-              Total Donations in USD ~${totalDonationsInUSD}
-            </span>
-          </div>
+      {chainId != null && (chainId == 80001 || chainId == 137) && (
+        <div className="global-donations">
+          Global donations : $
+          {globalDonationsPolygon && globalDonationsPolygon[0]}
         </div>
       )}
+
+      {chainId != null &&
+        (chainId == 65 ||
+          chainId == 66 ||
+          chainId == 4002 ||
+          chainId == 250) && (
+          <div className="global-donations okc-global--donation">
+            Global donations (Hover here)
+            <br />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                flexDirection: "column",
+              }}
+            >
+              {globalDonationsOKX.length > 0 &&
+                globalDonationsOKX.map((item, index) => (
+                  <span
+                    key={index}
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      padding: "0 20px",
+                    }}
+                  >
+                    <span
+                      style={{
+                        width: "40%",
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        textAlign: "left",
+                      }}
+                    >
+                      {item?.donationTokenSymbol}{" "}
+                      <img
+                        src={item?.donationTokenImage}
+                        alt={item?.donationTokenSymbol}
+                        width={"20px"}
+                      />
+                    </span>{" "}
+                    <span
+                      style={{
+                        fontWeight: "500",
+                        textDecoration: "underline",
+                        cursor: "pointer",
+                        textAlign: "left",
+                      }}
+                      title={item?.donatedTokenAmount}
+                    >
+                      {" "}
+                      {item?.donatedTokenAmount.substr(0, 8) + "..."}
+                    </span>
+                  </span>
+                ))}
+              <span
+                style={{
+                  margin: "0 auto",
+                }}
+              >
+                Total Donations in USD ~${totalDonationsInUSD}
+              </span>
+            </div>
+          </div>
+        )}
       {/* <div className="inventory">
             <div>Inventory</div>
             <div > <img className="inventory-item" src="https://i.ibb.co/CWfhL6J/image.png"/> 10</div>
